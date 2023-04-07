915e7054803bedcd02017da839ccf19cbf69c036
kalleolumets <olumetskalle@gmail.com>
2023-04-07 13:57:16 +0300

    Login routine first design in React UI

--- ClientApp/src/components/Login/Login.js
+++ ClientApp/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 ﻿//import * as React from 'react';
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -20,7 +20,13 @@ const theme = createTheme();
 
 export default function Login() {
 
-    const [showStatus, setShowStatus] = useState(-1); 
+    const [showStatus, setShowStatus] = useState(-1);
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        setUser(localStorage.getItem("user"))
+        
+    }, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -29,66 +35,103 @@ export default function Login() {
             email: data.get('email'),
             password: data.get('password'),
         });
-        if (data.get('email')) {
+        if (data.get('email') == "kasutaja") {
             localStorage.setItem('user', JSON.stringify(data.get('email')));
+
         }
         
     };
 
+    const handleLogout = (event) => {
+
+        console.log("Logout")
+        localStorage.removeItem("user");
+
+
+    };
+
+    const renderReturn = () => {
+
+        if (user) {
+
+            return (
+                <h1>
+                    {user}
+                    <Button
+                        onClick={handleLogout}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+
+                    >Logout</Button>
+                </h1>
+            );
+            
+        } else {
+            
+                return (
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+          </Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Kasutajanimi"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Salasõna"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+
+                            >
+                                Logige sisse
+            </Button>
+
+                        </Box>
+                    </Box>
+
+                </Container>
+            
+            );
+        }
+
+    }
+
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-          </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Kasutajanimi"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Salasõna"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                       
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            
-                        >
-                            Logige sisse
-            </Button>
-                        
-                    </Box>
-                </Box>
-               
-            </Container>
+            {renderReturn()}
         </ThemeProvider>
     );
 }
