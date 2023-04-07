385c085604c94f3276d3269f0b467da1349ec25d
kalleolumets <olumetskalle@gmail.com>
2023-04-07 18:54:08 +0300

    Using axios for API calls

--- ClientApp/src/components/FeeTypes.js
+++ ClientApp/src/components/FeeTypes.js
@@ -1,4 +1,23 @@
 import React, { Component, useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import axios from "axios";
+
+
+const style = {
+    position: 'absolute',
+    top: '30%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    bgcolor: 'background.paper',
+    border: '1px  #000',
+    boxShadow: 4,
+    p: 4,
+};
 
 
 export default function FeeTypes() {
@@ -6,6 +25,11 @@ export default function FeeTypes() {
     const [displayName, setDisplauName] = useState("Fee Types");
     const [user, setUser] = useState();
     const [feeTypes, setFeeTypes] = useState([]);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+   
 
     const populateFeeTypes = async () => {
         const response = await fetch('api/mfeetypes/feetypes');
@@ -13,17 +37,68 @@ export default function FeeTypes() {
         setFeeTypes(data);
     }
 
+    const retrieveFeeTypes = async () => {
+        await axios.get("https://localhost:44450/api/mfeetypes/feetypes")
+            .then((response) => {
+                console.log(response.data);
+                setFeeTypes(response.data)
+            })
+    }
+
 
     useEffect(() => {
         setUser(localStorage.getItem("user"))
-        populateFeeTypes()
+        //populateFeeTypes()
+        retrieveFeeTypes()
     }, []);
+
+
+    const renderModal = () => {
+        return (
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        
+                    </Typography>
+                    Uue makseviisi lisamine
+                        <TextField id="outlined-basic" label="makseviis" variant="outlined" />
+                    <TextField id="filled-basic" label="märkused" variant="outlined" />
+                    <Button
+                        onClick={handleClose}
+                        type="submit"
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+
+                    >Salvesta</Button>
+                </Box>
+            </Modal>
+        );
+    }
 
     const renderFeeTypesTable = (ftypes) => {
 
         if (user) {
 
             return (
+                <div>
+                    <Button
+                        onClick={handleOpen}
+                        type="submit"                    
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+
+                    >Lisa uus makseviis</Button>
+                    {renderModal()}
+
+
                 <table className="table table-striped" aria-labelledby="tableLabel">
                     <thead>
                         <tr>
@@ -42,11 +117,16 @@ export default function FeeTypes() {
                             </tr>
                         )}
                     </tbody>
-                </table>
+                    </table>
+                </div>
             )
         } else {
             return (
-                <h1>Puudub teabevajadus</h1>
+                <div>
+                    <h1>Puudub teabevajadus</h1>
+                    
+                </div>
+                
             )
         }
 
@@ -55,10 +135,10 @@ export default function FeeTypes() {
  
 
     return (
-      <div>
+        <div>
+         
         <h1 id="tableLabel">Maksevõimalused</h1>
-      
-            
+         
             {renderFeeTypesTable(feeTypes)}
       </div>
     );
