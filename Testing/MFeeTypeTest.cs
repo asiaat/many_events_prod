e4db0deb9a44d70dcc431e146fdcd7fdff2af4ca
kalleolumets <olumetskalle@gmail.com>
2023-04-11 00:35:59 +0300

    Testing framework

---
+++ Testing/MFeeTypeTest.cs
@@ -0,0 +1,34 @@
+﻿
+using ManyEvents.Controllers;
+using Xunit;
+using System;
+using ManyEvents.Models;
+
+namespace Testing;
+
+public class MFeeTypeTest : IDisposable
+{
+    MFeeType testCase;
+
+    public MFeeTypeTest()
+    {
+        testCase = new MFeeType();
+        testCase.SetName("sularaha");
+
+    }
+
+    
+    public void Dispose()
+    {
+        // close down
+
+        // close the DB
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal("sularaha", testCase.Name);
+    }
+
+}
